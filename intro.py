21cdff03f9cd6395c21c0b66515ea87fbe8d85d9
PhaniChinna <116142474+PhaniChinna@users.noreply.github.com>
2025-01-02 22:42:58 +0530

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,66 +1,119 @@
+# print("Hello World")
+# print("###")
 
-print("Hello World")
-print("###")
+# Addition
 
-Addition
+# Digit_one = 2495
+# Digit_Two = 789358
+# print(Digit_one + Digit_Two)
 
-Digit_one = 2495
-Digit_Two = 789358
-print(Digit_one + Digit_Two)
-
-Substraction
-Digit_One = 596
-Digit_Two = 193856
-print(Digit_Two - Digit_One)
+# Substraction
+# Digit_One = 596
+# Digit_Two = 193856
+# print(Digit_Two - Digit_One)
 
 
-Product
-Digit_One = 37
-Digit_Two = 61
-Digit_Three = 391
-print(Digit_One * Digit_Two * Digit_Three)
+# Product
+# Digit_One = 37
+# Digit_Two = 61
+# Digit_Three = 391
+# print(Digit_One * Digit_Two * Digit_Three)
 
-Divisible
-Digit_One = 33968
-Digit_Two = 176
-print(Digit_One / Digit_Two)
+# Divisible
+# Digit_One = 33968
+# Digit_Two = 176
+# print(Digit_One / Digit_Two)
 
-a = int(input("Please Enter a Number One "))
-b = int(input("Please Enter a Number Two "))
-result = a + b
-print("Sum : " + str(result))
+# a = int(input("Please Enter a Number One "))
+# b = int(input("Please Enter a Number Two "))
+# result = a + b
+# print("Sum : " + str(result))
 
-Rectangle l * W
+# Rectangle l * W
 
-Length_of_Rectangle = int(input("Enter Length of Rectangle : "))
-Width_of_Rectangle = int(input("Enter Width of Rectangle : "))
-Area_of_Rectangle = Length_of_Rectangle * Width_of_Rectangle
-print(Area_of_Rectangle)
+# Length_of_Rectangle = int(input("Enter Length of Rectangle : "))
+# Width_of_Rectangle = int(input("Enter Width of Rectangle : "))
+# Area_of_Rectangle = Length_of_Rectangle * Width_of_Rectangle
+# print(Area_of_Rectangle)
 
-Square l * W
+# Square l * W
 
-length_of_Square = int(input("Enter a Length of Square : "))
-Width_of_Square = int(input("Enter a Width of Square : "))
-Area_of_Square = length_of_Square * Width_of_Square
-print(Area_of_Square)
+# length_of_Square = int(input("Enter a Length of Square : "))
+# Width_of_Square = int(input("Enter a Width of Square : "))
+# Area_of_Square = length_of_Square * Width_of_Square
+# print(Area_of_Square)
 
-Circle pie r^2
+# Circle pie r^2
 
-Radius_of_Circle = int(input())
-Area_of_Circle = 3.14 * Radius_of_Circle ** 2
-print(Area_of_Circle)
+# Radius_of_Circle = int(input())
+# Area_of_Circle = 3.14 * Radius_of_Circle ** 2
+# print(Area_of_Circle)
 
-Triangle 1/2* (b * h)
+# Triangle 1/2* (b * h)
 
-Breath_of_Triangle = int(input("Enter a Breath of Triangle: "))
-height_of_Triangle = int(input("Enter a height of Triangle: "))
-Area_of_Triangle = 1/2 * Breath_of_Triangle * height_of_Triangle
-print(Area_of_Triangle)
+# Breath_of_Triangle = int(input("Enter a Breath of Triangle: "))
+# height_of_Triangle = int(input("Enter a height of Triangle: "))
+# Area_of_Triangle = 1/2 * Breath_of_Triangle * height_of_Triangle
+# print(Area_of_Triangle)
 
-Parallelogram  b * h
+# Parallelogram  b * h
 
-Breath_of_Parallelogram = int(input("Enter a Breath Number: "))
-Height_of_Parallelogram = int(input("Enter a Height Number: "))
-Area_of_Parallellogram = Breath_of_Parallelogram * Height_of_Parallelogram
-print(Area_of_Parallellogram)
+# Breath_of_Parallelogram = int(input("Enter a Breath Number: "))
+# Height_of_Parallelogram = int(input("Enter a Height Number: "))
+# Area_of_Parallellogram = Breath_of_Parallelogram * Height_of_Parallelogram
+# print(Area_of_Parallellogram)
 
+# Trapezoid 1/2* (a + b) * h
+
+# Area_of_Trapezoid = int(input("Enter a Area Number : "))
+# Breath_of_Trapezoid = int(input("Enter a Breath Number :"))
+# Height_of_Trapezoid = int(input("Enter a Height Number : "))
+# Result_of_Trapezoid = 1/2 * \
+#     (Area_of_Trapezoid + Breath_of_Trapezoid) * Height_of_Trapezoid
+# print(Result_of_Trapezoid)
+
+# if Condition :
+# A = int(input("Enter a Number A : "))
+# B = int(input("Enter a Number B : "))
+# if (A > B):
+#     print(A - B)
+# print(A + B)
+
+# w = "NxtWave"
+# print(w[0] + w[6])
+
+# Word = input("Enter a String Input : ")
+# length_of_String = len(Word) - 2
+# First_Character = Word[0]
+# Last_Character = Word[len(Word) - 1]
+# Stars = "*" * length_of_String
+# print(First_Character + Stars + Last_Character)
+
+# W = "Practice"
+# length = len(W)
+# result = (length % 2)
+# print(result)
+
+
+# if(False):
+#     print("Hi")
+# else:
+#     if(False):
+#         print("Hlo")
+#     print("Hello Welcome")
+# print("Bye")
+
+
+# Num_One = int(input("Enter First Number: "))
+# Num_Two = int(input("Enter Second Number: "))
+# Num_Three = int(input("Enter Third Number: "))
+# is_Greater_A = (Num_One > Num_Two) and (Num_Two > Num_Three)
+
+# if(is_Greater_A):
+#     print(Num_One)
+# else:
+#     is_Greater_B = Num_Two > Num_Three
+#     if(is_Greater_B):
+#         print(Num_Two)
+#     else:
+#         print(Num_Three)
